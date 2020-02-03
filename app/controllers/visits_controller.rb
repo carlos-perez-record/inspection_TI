338e1472ac66f0973e24e0a04a046ea9c67589c5
carlos-perez-record <carlos.perez@record.com.co>
2020-02-03 16:35:33 -0500

    Publicar Visita parte 1

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -2,4 +2,8 @@ class VisitsController < ApplicationController
   def index
     @visits = Visit.all
   end
+
+  def new
+    @visits = Visit.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'visits/index'
+  get '/visits/new', to: 'visits#new'
   root 'visits#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
