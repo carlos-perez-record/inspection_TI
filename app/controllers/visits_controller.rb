20a6843b664e5a08ed885e95f7862eb0c7d8e46a
carlos-perez-record <carlos.perez@record.com.co>
2020-02-03 16:48:29 -0500

    Publicar visita parte 2

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,9 +1,30 @@
 class VisitsController < ApplicationController
+
+  # Se crea metodo index
   def index
     @visits = Visit.all
   end
 
+  # Se crea metodo index
   def new
-    @visits = Visit.new
+    @visit = Visit.new
   end
+
+  # Se crea metodo create
+  def create
+    @visit = Visit.new(visit_params)
+    if @visit.save
+      redirect_to visits_path, notice: "La Visita fue publicada con éxito"
+    else
+      render :new
+    end
+  end
+
+
+  private
+  # Metodo privado params
+  def visit_params
+    params.require(:visit).permit(:fecha)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'visits/index'
-  get '/visits/new', to: 'visits#new'
+  get '/visits/new', to: 'visits#new', as: 'new_product'
+  post "visits", to: 'visits#create'
   root 'visits#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
