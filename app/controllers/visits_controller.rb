20050c9ad3d4391a8fc499a8f3a515dbac12660a
carlos-perez-record <carlos.perez@record.com.co>
2020-02-11 17:06:58 -0500

    Se implementa metodo destroy

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -29,12 +29,18 @@ class VisitsController < ApplicationController
   end
 
   def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
-      redirect_to products_path, notice: "El producto ha sido modificado con éxito"
+    @visit = Visit.find(params[:id])
+    if @visit.update(visit_params)
+      redirect_to visits_index_path, notice: "La visita ha sido modificado con éxito"
     else
       render :edit
     end
+  end
+
+  def destroy
+    visit = Visit.find(params[:id])
+    visit.destroy
+    redirect_to visits_index_path, notice: "La visita fue eliminada con éxito"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'visits/index'
-  get '/visits/new', to: 'visits#new', as: 'new_product'
+  get '/visits/new', to: 'visits#new', as: 'new_visit'
   get '/visits/:id', to: 'visits#show'
   get '/visits/:id', to: 'visits#show', as: 'visit'
-  get '/visits/:id/edit', to: 'visits#edit'
-  patch '/products/:id', to: 'products#update'
+  get '/visits/:id/edit', to: 'visits#edit', as: 'edit_visit'
+  patch '/visits/:id', to: 'visits#update'
   post "visits", to: 'visits#create'
+  delete '/visits/:id', to: 'visits#destroy'
   root 'visits#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
